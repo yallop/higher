beea0f797f853a4f523f873d071a378b5cd189a2
Jeremy Yallop <yallop@gmail.com>
2013-11-16 15:46:33 +0000

    Remove argument kinds.

--- newtype.mli
+++ newtype.mli
@@ -14,30 +14,29 @@ Brands are created by the {!Param} and {!Param2} functors.  Applications are
 instantiations of the {!app} type constructor.
 *)
 
-type ('rep, 'brand, 'kind) newtype
+type ('rep, 'brand) newtype
 (** A description of a newtype.  The components are
-- rep, the representation type
+
+- rep, the representation type, which has the form
+      arg1 -> arg2 -> ... -> argn -> t star
+  for some types arg1 ... argn, t.
+
 - brand, a newtype expression 
-- kind, the kind of the newtype.  See {!star} below
 *)
+
+type _ star
+(** The kind of types. *)
 
 type ('p, 'f) app
 (** Type expression application. *)
 
-(** There is a kind system built from {!star}, the base kind, and arrow. *)
-
-type star
-(** The kind of types. *)
-
-val inj : ('rep, 'brand, star) newtype -> 'rep -> 'brand
+val inj : ('rep star, 'brand) newtype -> 'rep -> 'brand
 (** Inject a value into a newtype.  Injection operates at kind {!star} *)
 
-val prj : ('rep, 'brand, star) newtype -> 'brand -> 'rep
+val prj : ('rep star, 'brand) newtype -> 'brand -> 'rep
 (** Projection a value from a newtype.  Projection operates at kind {!star} *)
 
-val (~~) :
-  ('a -> 'repr, 'brand, _ -> 'kind) newtype ->
-  ('repr, ('a, 'brand) app, 'kind) newtype
+val (~~) : ('a -> 'repr, 'brand) newtype -> ('repr, ('a, 'brand) app) newtype
 (** Instantiation of a parameterized newtype constructor.  For example, given a
 constructor for lists
 
@@ -55,14 +54,14 @@ and the weak type variables will be filled in by unification.
 *)
 
 
-module Param (S : sig type 'a repr type argkind end) : sig
+module Param (S : sig type 'a repr end) : sig
   type t
-  val t : ('a -> 'a S.repr, t, S.argkind -> star) newtype
+  val t : ('a -> 'a S.repr star, t) newtype
 end
 (** Construct a newtype for a type constructor with one parameter. *)
 
-module Param2 (S : sig type ('a, 'b) repr type argkind1 type argkind2 end) : sig
+module Param2 (S : sig type ('a, 'b) repr end) : sig
   type t
-  val t : ('a -> 'b -> ('a, 'b) S.repr, t, S.argkind1 -> S.argkind2 -> star) newtype
+  val t : ('a -> 'b -> ('a, 'b) S.repr star, t) newtype
 end
 (** Construct a newtype for a type constructor with two parameters. *)
