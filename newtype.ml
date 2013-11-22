7f69a771aec6e60ed410d93377a196b8a306495a
Leo White <lpw25@cl.cam.ac.uk>
2013-11-22 10:47:36 +0000

    Separate the iso type and add a few more newtype functors

--- newtype.ml
+++ newtype.ml
@@ -1,37 +1,64 @@
+open Iso
+
 (* Representation of type application.  This corresponds to the "apply"
    variant type in a defunctionalized program.  Application is postfix. *)
 type ('p, 'f) app = ..
 
-(* Isomorphisms. *)
-type ('a, 'b) iso = { inj : 'a -> 'b; prj : 'b -> 'a; }
-
-(* Injection operates at kind Star. *)
-let inj : 'a 'b. ('a, 'b) iso -> 'a -> 'b =
-  fun {inj} -> inj
-
-(* Projection operates at kind Star. *)
-let prj : 'a 'b. ('a, 'b) iso -> 'b -> 'a =
-  fun {prj} -> prj
-
-(* Some useful constructors for building newtypes *)
-let primitive = { inj = (fun x -> x); prj = (fun x -> x) }
-
-module Param (S : sig type 'a repr end) : sig
+module Newtype0 (T : sig type t end) : sig
   type t
-  val t : ('a S.repr, ('a, t) app) iso
+  val t : (T.t, t) iso
+end =
+struct
+  type t = Id of T.t
+  let t = { inj = (fun v -> Id v); prj = (fun (Id v) -> v) }
+end
+
+module Newtype1 (T : sig type 'a t end) : sig
+  type t
+  val t : ('a T.t, ('a, t) app) iso
 end =
 struct
   type t
-  type (_, _) app += App : 'a S.repr -> ('a, t) app
+  type (_, _) app += App : 'a T.t -> ('a, t) app
   let t = { inj = (fun v -> App v); prj = (fun (App v) -> v) }
 end
 
-module Param2 (S : sig type ('a, 'b) repr end) : sig
+module Newtype2 (T : sig type ('a, 'b) t end) : sig
   type t
-  val t : (('a, 'b) S.repr, ('b, ('a, t) app) app) iso
+  val t : (('a, 'b) T.t, ('b, ('a, t) app) app) iso
 end =
 struct
   type t
-  type (_, _) app += App : ('a, 'b) S.repr -> ('b, ('a, t) app) app
+  type (_, _) app += App : ('a, 'b) T.t -> ('b, ('a, t) app) app
+  let t = { inj = (fun v -> App v); prj = (fun (App v) -> v) }
+end
+
+module Newtype3 (T : sig type ('a, 'b, 'c) t end) : sig
+  type t
+  val t : (('a, 'b, 'c) T.t, ('c, ('b, ('a, t) app) app) app) iso
+end =
+struct
+  type t
+  type (_, _) app += App : ('a, 'b, 'c) T.t -> ('c, ('b, ('a, t) app) app) app
+  let t = { inj = (fun v -> App v); prj = (fun (App v) -> v) }
+end
+
+module Newtype4 (T : sig type ('a, 'b, 'c, 'd) t end) : sig
+  type t
+  val t : (('a, 'b, 'c, 'd) T.t, ('d, ('c, ('b, ('a, t) app) app) app) app) iso
+end =
+struct
+  type t
+  type (_, _) app += App : ('a, 'b, 'c, 'd) T.t -> ('d, ('c, ('b, ('a, t) app) app) app) app
+  let t = { inj = (fun v -> App v); prj = (fun (App v) -> v) }
+end
+
+module Newtype5 (T : sig type ('a, 'b, 'c, 'd, 'e) t end) : sig
+  type t
+  val t : (('a, 'b, 'c, 'd, 'e) T.t, ('e, ('d, ('c, ('b, ('a, t) app) app) app) app) app) iso
+end =
+struct
+  type t
+  type (_, _) app += App : ('a, 'b, 'c, 'd, 'e) T.t -> ('e, ('d, ('c, ('b, ('a, t) app) app) app) app) app
   let t = { inj = (fun v -> App v); prj = (fun (App v) -> v) }
 end
--- newtype.mli
+++ newtype.mli
@@ -1,27 +1,42 @@
 (** Operations on newtypes *)
 
-(** An isomorphism between types *)
-type ('a, 'b) iso
+open Iso
 
 (** Type expression application. *)
 type ('p, 'f) app
 
-(** Inject a value using an isomorphism *)
-val inj : ('a, 'b) iso -> 'a -> 'b
-
-(** Project a value using an isomorphism *)
-val prj : ('a, 'b) iso -> 'b -> 'a
-
-val primitive : ('a, 'a) iso
+(** Construct a newtype for a type constructor with no parameters. *)
+module Newtype0 (T : sig type t end) : sig
+  type t
+  val t : (T.t, t) iso
+end
 
 (** Construct a newtype for a type constructor with one parameter. *)
-module Param (S : sig type 'a repr end) : sig
+module Newtype1 (T : sig type 'a t end) : sig
   type t
-  val t : ('a S.repr, ('a, t) app) iso
+  val t : ('a T.t, ('a, t) app) iso
 end
 
 (** Construct a newtype for a type constructor with two parameters. *)
-module Param2 (S : sig type ('a, 'b) repr end) : sig
+module Newtype2 (T : sig type ('a, 'b) t end) : sig
   type t
-  val t : (('a, 'b) S.repr, ('b, ('a, t) app) app) iso
+  val t : (('a, 'b) T.t, ('b, ('a, t) app) app) iso
+end
+
+(** Construct a newtype for a type constructor with three parameters. *)
+module Newtype3 (T : sig type ('a, 'b, 'c) t end) : sig
+  type t
+  val t : (('a, 'b, 'c) T.t, ('c, ('b, ('a, t) app) app) app) iso
+end
+
+(** Construct a newtype for a type constructor with four parameters. *)
+module Newtype4 (T : sig type ('a, 'b, 'c, 'd) t end) : sig
+  type t
+  val t : (('a, 'b, 'c, 'd) T.t, ('d, ('c, ('b, ('a, t) app) app) app) app) iso
+end
+
+(** Construct a newtype for a type constructor with five parameters. *)
+module Newtype5 (T : sig type ('a, 'b, 'c, 'd, 'e) t end) : sig
+  type t
+  val t : (('a, 'b, 'c, 'd, 'e) T.t, ('e, ('d, ('c, ('b, ('a, t) app) app) app) app) app) iso
 end
